0a9276f100510baddd9ac3899711e2aefb83b735
Yuki Kubota <rakudalms@gmail.com>
2021-10-03 13:24:41 +0900

    indexAction スクレイピング関連を除外して軽量化(今後別のメソッドにする予定)

--- api/app/controllers/api/holdings_controller.rb
+++ api/app/controllers/api/holdings_controller.rb
@@ -2,6 +2,14 @@ module Api
   class HoldingsController < ApplicationController
     #最終的に毎回スクレイピングしなくていいようにリファクタリング予定
     def index
+      holdings = Holding.order(total_dividend_amount: :asc)
+      render json: holdings
+    end
+
+
+
+
+    def get_dividend
       agent = Mechanize.new
       holdings = Holding.all
       holdings.map { |holding|
@@ -10,13 +18,10 @@ module Api
         current_div_rate = individual_page.search("td")[118].text.to_f
         holding.update(
           dividend_amount: current_div_amount,
-          dividend_rate: current_div_rate,
+          dividend_: current_div_rate,
           total_dividend_amount: current_div_amount * holding.quantity,
         )
       }
-      render json: holdings.order(total_dividend_amount: :desc)
     end
-
-
   end
 end
