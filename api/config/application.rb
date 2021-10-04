e59a7a04286fc1ef1011688a33db982f9aba7130
Yuki Kubota <rakudalms@gmail.com>
2021-10-04 22:32:14 +0900

    factory_bot&database_cleaner導入時の設定

--- api/config/application.rb
+++ api/config/application.rb
@@ -50,5 +50,17 @@ module High10Io
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    #factory_botの設定
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: true
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
