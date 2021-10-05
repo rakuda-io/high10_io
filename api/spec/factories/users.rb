9cdf512d053e88353aaed3bc3a6cd3b9b67df239
Yuki Kubota <rakudalms@gmail.com>
2021-10-05 12:13:20 +0900

    Holdingのindexアクションのテスト実装

--- api/spec/factories/users.rb
+++ api/spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "test_#{n}" }
     sequence(:email) { |n| "test+#{n}@example.com" }
-    password { "password" }
-    password_confirmation { "password" }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
